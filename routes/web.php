d45e56ad85b93f1d2b7d062e7cb6eb5110333702
mensafri <safrijawad1245@gmail.com>
2023-08-12 16:34:06 +0700

    add comment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\IdeasController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('/ideas', IdeasController::class);
+Route::resource('/comment', CommentController::class);
 Route::get('/', [IdeasController::class, 'index']);
